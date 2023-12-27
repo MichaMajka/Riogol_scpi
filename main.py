d8f16027fb0c004e05404b04a2446ba1cd2d45db
Michal <m.a.lipczynski@gmail.com>
2023-12-27 15:14:38 +0100

    next step will be processing data to measure how long voltage was increasing from 0 to 10V

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pyvisa
 import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 rm = pyvisa.ResourceManager()
 print(rm.list_resources())
@@ -22,6 +24,7 @@ osciloscope.write(":SINGLE")
 
 # Wait and Check for Trigger
 triggered = False
+print("start measuring")
 while not triggered:
     response = osciloscope.query(":TRIGger:POSition?").strip()
     if response != '-2':
@@ -32,6 +35,46 @@ time.sleep(1)
 max_value_str = osciloscope.query(':MEASure:ITEM? VMAX,CHANnel1')
 max_value_float = float(max_value_str.strip())
 print(max_value_float)
+
+# Capture the waveform data
+osciloscope.write(":WAVEFORM:FORMAT ASCII")
+osciloscope.write(":WAVEFORM:SOURCE CHANNEL1")
+osciloscope.write(":WAVEFORM:MODE NORMAL")
+raw_data = osciloscope.query(":WAVEFORM:DATA?")
+header_length = 2 + int(raw_data[1])  # '#9' + number of digits in the length field
+data_string = raw_data[header_length:]
+waveform_data = np.array([float(val) for val in data_string.split(',')])
+
+
+
+# Time per division and number of divisions (adjust these based on your oscilloscope settings)
+time_per_div = 0.5  # seconds per division
+num_divisions = 10  # total number of horizontal divisions on the screen
+total_time = time_per_div * num_divisions
+time_step = total_time / len(waveform_data)
+times = np.arange(0, total_time, time_step)
+
+# Find the indices where voltage crosses 5V and 10V
+index_0v = np.where(waveform_data >= 0)[0][0]
+index_5v = np.where(waveform_data >= 5)[0][0]
+index_10v = np.where(waveform_data >= 10)[0][0]
+
+# Calculate the time difference
+time_0v = times[index_0v]
+time_5v = times[index_5v]
+time_10v = times[index_10v]
+time_difference = time_10v - time_5v
+
+print(f"Time from 5V to 10V: {time_difference} seconds")
+
+# Plot the waveform
+plt.figure(figsize=(10, 6))
+plt.plot(times, waveform_data)
+plt.title('Oscilloscope Waveform')
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (V)')
+plt.grid(True)
+plt.show()
 
 # Close the connection
 osciloscope.close()
